09e4d1962d8adb925c4c38cc2d4a58d9edaaf931
angryz <angryzorro@gmail.com>
2016-11-07 14:29:15 +0800

    Practice OOP with js

--- js/oo/class.js
+++ js/oo/class.js
@@ -17,6 +17,10 @@ Person.prototype.sayHello = function () {
     console.log("Hello, I'm " + this.firstName);
 };
 
+Person.prototype.walk = function () {
+    console.log("I'm walking.");
+}
+
 // Object ( Instance of Class )
 var person1 = new Person("Alice");
 var person2 = new Person("Bob");
@@ -26,3 +30,29 @@ person2.sayHello();
 
 var helloFunc = person1.sayHello;
 helloFunc.call(person2);
+
+
+// Extent
+function Student(firstName, subject) {
+    // 调用父类构造器, 确保(使用Function#call)"this" 在调用过程中设置正确
+    Person.call(this, firstName);
+
+    // 初始化Student类特有属性
+    this.subject = subject;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.sayHello = function () {
+    console.log("Hello, I'm " + this.firstName + ". I'm studying " + this.subject);
+};
+
+Student.prototype.sayGoodBye = function () {
+    console.log("Goodbye!");
+}
+
+var student1 = new Student("Janet", "Applied Physics");
+student1.sayHello();
+student1.walk();
+student1.sayGoodBye();
